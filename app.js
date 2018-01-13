a8ffcb8deb5e8b3b87317fa95ee3c66595863b1f
laffingragons <akshaypatil,vit@gmail.com>
2018-01-14 01:47:33 +0530

    intilization

---
+++ app.js
@@ -0,0 +1,117 @@
+var express = require('express');
+var app = express();
+// module for maintaining sessions
+var session = require('express-session');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var mongoose = require('mongoose');
+
+// path is used the get the path of our files on the computer
+var path = require ('path');
+
+app.use(logger('dev'));
+app.use(bodyParser.json({limit:'10mb',extended:true}));
+app.use(bodyParser.urlencoded({limit:'10mb',extended:true}));
+app.use(cookieParser());
+
+// initialization of session middleware 
+
+app.use(session({
+  name :'myCustomCookie',
+  secret: 'myAppSecret', // encryption key 
+  resave: true,
+  httpOnly : true,  // to aviod cookie forgery
+  saveUninitialized: true,
+  cookie: { secure: false } //make true for ssl
+}));
+
+// set the templating engine 
+app.set('view engine', 'jade');
+
+//set the views folder
+app.set('views', path.join(__dirname + '/app/views'));
+
+
+var dbPath = "mongodb://localhost/LinkInDB";
+//command to connect with database
+db = mongoose.connect(dbPath);
+
+mongoose.connection.once('open', function () {
+
+    console.log("Database Connection open successfully");
+});
+
+
+// fs module, by default module for file management in nodejs
+var fs = require('fs');
+
+// include all our model files
+fs.readdirSync('./app/models').forEach(function (file) {
+    // check if the file is js or not
+    if (file.indexOf('.js'))
+        // if it is js then include the file from that folder into our express app using require
+        require('./app/models/' + file);
+
+}); // end for each
+
+// include controllers
+fs.readdirSync('./app/controllers').forEach(function (file) {
+    if (file.indexOf('.js')) {
+        // include a file as a route variable
+        var route = require('./app/controllers/' + file);
+        //call controller function of each file and pass your app instance to it
+        route.controllerFunction(app)
+
+    }
+
+}); //end for each
+
+//including auth file here
+
+var auth = require('./middlewares/auth');
+var mongoose = require('mongoose');
+var userModel = mongoose.model('User');
+
+//set tthe middleware as an application level middleware
+app.use(function(req,res,next){
+    
+    //its checking wether this session and session.user exist or not
+    
+    //to check if its a legitimate user of the system
+	if(req.session && req.session.user){
+		userModel.findOne({'email':req.session.user.email},function(err,user){
+
+			if(user){
+                //setting another variable here
+				/*req.user = user;
+				delete req.user.password;*/ 
+				req.session.user = user;
+                //deleting the password
+                delete req.session.user.password;
+				next()
+			}
+			else{
+				// do nothing , because this is just to set the values
+			}
+		});
+	}
+	else{
+		next();
+	}
+
+
+});
+
+
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: err
+    });
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
